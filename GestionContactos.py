0e50718f6ae273cf9096154bbad55d702ed647a7
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-27 23:59:07 -0500

    Update GestionContactos.py

--- GestionContactos.py
+++ GestionContactos.py
@@ -2,3 +2,9 @@ def __init__(self):
         self.contactos = {}
 def agregar_contacto(self, nombre, telefono):
         self.contactos[nombre] = telefono
+
+def buscar_contacto(self, nombre):
+    if nombre in self.contactos:
+        return f"{nombre}: {self.contactos[nombre]}"
+    else:
+        return "Contacto no encontrado."
