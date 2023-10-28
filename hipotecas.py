d4f266888bb754060e3245648dbe95acaff53799
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-27 23:56:05 -0500

    Update hipotecas.py

--- hipotecas.py
+++ hipotecas.py
@@ -2,4 +2,4 @@ def calcular_pago_hipoteca(monto_prestamo, tasa_interes_anual, plazo_anios):
      """
     Esta función calcula el pago mensual de una hipoteca.
     """
-    
+    tasa_interes_mensual = (tasa_interes_anual / 100) / 12
